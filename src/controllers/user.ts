06378754c6f09b46385f8d3b5d3a7cc8f2e53ebe
Ákos Lenkei <Akos.Lenkei@ibm.com>
2022-11-25 14:53:14 +0100

    Authentication wip3 cript pswd

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -3,26 +3,31 @@ import { ICreateUserRequest } from '../interfaces';
 import { IUser, UserModel } from '../models/User';
 
 export async function createUser(req: ICreateUserRequest, res: Response, next: NextFunction): Promise<Response | void> {
-    if (!req.body.email) {
+    const { email, password } = req.body;
+    if (!email) {
         return next({ status: 400, message: 'Not valid email' });
     }
-    if (!req.body.password) {
+    if (!password) {
         return next({ status: 400, message: 'Not valid password' });
     }
     try {
+        const userExistsAlready: IUser | null = await UserModel.findOne({ email });
+        if (userExistsAlready) {
+            return next({ status: 400, message: 'This user email already exists.' });
+        }
         await UserModel.create(req.body);
         return res.send('ok');
     } catch (err) {
         return next({
             status: 500,
-            message: 'Failed to create user',
+            message: err,
         });
     }
 }
 
 export async function listUsers(req: Request, res: Response, next: NextFunction): Promise<Response<IUser[]> | void> {
     try {
-        const users: IUser[] = await UserModel.find({});
+        const users: IUser[] = await UserModel.find({}, { email: 1 });
         return res.json(users);
     } catch (err) {
         return next({ message: 'Could not get users', status: 500 });
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,11 +1,15 @@
-import {model, Schema} from 'mongoose';
+import { CallbackError, model, Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
 export interface IUser {
     readonly email: string;
     readonly password: string;
 }
-const UserSchema = new Schema({
-    email: {type: String, required: true},
-    password: {type: String, required: true},
+const UserSchema = new Schema<IUser>({
+    email: { type: String, required: true },
+    password: { type: String, required: true },
 });
-
+UserSchema.pre('save', async function (next: (err?: CallbackError) => void) {
+    this.password = await bcrypt.hash(this.password, 8);
+    next();
+});
 export const UserModel = model('User', UserSchema);
